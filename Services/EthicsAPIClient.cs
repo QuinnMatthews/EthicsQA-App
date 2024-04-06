5a32564857a8fff4c989622cdf1e046a8b660935
Matthews, Quinn <Quinn.Matthews@lrs.com>
2024-04-06 10:27:58 -0500

    Scaffold API Client

---
+++ Services/EthicsAPIClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace EthicsQA.Services {
+    public class EthicsAPIClient {
+        public EthicsAPIClient() {
+        }
+
+        public async Task<LoginResult> LoginAsync() {
+            return new LoginResult();
+        }
+
+        public async Task LogoutAsync() {
+        }
+
+
+    }
+
+    public class LoginResult {
+        public bool IsError { get; set; }
+        public ClaimsPrincipal User { get; set; }
+    }
+}
